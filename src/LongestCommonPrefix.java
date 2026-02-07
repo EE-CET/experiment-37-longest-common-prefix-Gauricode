6480934dff9a3453b0bbd91e6cfdd9fd49f4068d
unknown <gaurijkjay@gmail.com>
2026-02-07 16:36:39 +0530

    re

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class LongestCommonPrefix {
-    
-        // TODO: Read N
-        // TODO: Handle the newline character after reading N
-        // TODO: Read N strings into an array
-        // TODO: Find the longest common prefix
-        // Hint: Start with the first string as the 'prefix'. 
-        // Iterate through the rest, shortening the 'prefix' until it matches the start of the current string.
-        // TODO: If prefix is empty, print "-1", otherwise print the prefix.
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+
+        String prefix = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            int j = 0;
+            while (j < prefix.length() && j < arr[i].length()
+                   && prefix.charAt(j) == arr[i].charAt(j)) {
+                j++;
+            }
+            prefix = prefix.substring(0, j);
+        }
+
+        if (prefix.length() == 0) {
+            System.out.print("-1");
+        } else {
+            System.out.print(prefix);
+        }
+    }
 }
